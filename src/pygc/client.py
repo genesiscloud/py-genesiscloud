c041369d2cb564d92ac3e67d91596aa91f1e6d04
Oz Tiram <otiram@genesiscloud.com>
2020-05-21 15:39:46 +0200

    Add the ability to create a resource
    This is a simple method - without breaks and safety. It assumes
    the user knows what he needs to pass. Otherwise, a clear error is
    returned from the API server.

--- src/pygc/client.py
+++ src/pygc/client.py
@@ -2,11 +2,47 @@ import sys
 
 import requests
 
+
+class APIError(Exception):
+    """An API Error Exception"""
+
+    def __init__(self, code, message):
+        self.code = code
+        self.message = message
+
+    def __str__(self):
+        return f"{self.message}"
+
+
 RESOURCES = {'Images': 'images',
              'Instances': 'instances',
              'SSHKeys': 'ssh-keys',
              'SecurityGroups': 'security-groups',
              'Snapshots': 'snapshots'}
+
+
+INSTANCE_TYPES = {
+    'vcpu-4_memory-12g_disk-80g_nvidia1080ti-1':
+    {"vCPUs": 4, "RAM":  12, "Disk": 80, "GPU": 1},
+    'vcpu-8_memory-24g_disk-80g_nvidia1080ti-2':
+    {"vCPUs": 8, "RAM":  24, "Disk": 80, "GPU": 2},
+    'vcpu-12_memory-36g_disk-80g_nvidia1080ti-3':
+    {"vCPUs": 12, "RAM": 36, "Disk": 80, "GPU": 3},
+    'vcpu-16_memory-48g_disk-80g_nvidia1080ti-4':
+    {"vCPUs": 16, "RAM": 48, "Disk": 80, "GPU": 4},
+    'vcpu-20_memory-60g_disk-80g_nvidia1080ti-5':
+    {"vCPUs": 20, "RAM": 60, "Disk": 80, "GPU": 5},
+    'vcpu-24_memory-72g_disk-80g_nvidia1080ti-6':
+    {"vCPUs": 24, "RAM": 72, "Disk": 80, "GPU": 6},
+    'vcpu-28_memory-84g_disk-80g_nvidia1080ti-7':
+    {"vCPUs": 28, "RAM": 84, "Disk": 80, "GPU": 7},
+    'vcpu-32_memory-96g_disk-80g_nvidia1080ti-8':
+    {"vCPUs": 32, "RAM": 96, "Disk": 80, "GPU": 8},
+    'vcpu-36_memory-108g_disk-80g_nvidia1080ti-9':
+    {"vCPUs": 36, "RAM": 108, "Disk": 80, "GPU": 9},
+    'vcpu-40_memory-120g_disk-80g_nvidia1080ti-10':
+    {"vCPUs": 40, "RAM": 120, "Disk": 80, "GPU": 10},
+}
 
 
 class GenesisResource:
@@ -19,13 +55,12 @@ class GenesisResource:
     def headers(self):
         return {"Content-Type": "application/json", "X-Auth-Token": self.apikey}
 
-    def __list(self, page=1, items=10, json=False, raw=False, **kwargs):
-        print(self.base_url + f"compute/v1/{self._route}")
+    def __list(self, page=1, items=10, json=False, raw=False, **kwargs):  # noqa
         response = requests.get(
-                self.base_url + f"compute/v1/{self._route}",
-                headers=self.headers,
-                params={"page": page, "per_page": items}
-                )
+            self.base_url + f"compute/v1/{self._route}",
+            headers=self.headers,
+            params={"page": page, "per_page": items}
+        )
 
         if json:
             yield response.json()['instances']
@@ -36,7 +71,7 @@ class GenesisResource:
             for item in response.json()[self._route.replace("-", "_")]:
                 yield item
 
-    def list(self, page=1, items=10, json=False, raw=False):
+    def list(self, page=1, items=10, json=False, raw=False):  # noqa
 
         if json:
             return next(self.__list(page=page, items=items, json=True))
@@ -44,6 +79,17 @@ class GenesisResource:
             return next(self.__list(page=page, items=items, raw=True))
         else:
             return self.__list(page=page, items=items)
+
+    def create(self, **kwargs):
+        response = requests.post(
+            self.base_url + f"compute/v1/{self._route}",
+            headers=self.headers,
+            json=kwargs
+        )
+        if response.status_code != 201:
+            raise APIError(response.status_code, response.content)
+
+        return response.json()
 
 
 for resource, route in RESOURCES.items():
