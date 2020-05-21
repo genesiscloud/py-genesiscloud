19b5244da47f53abb9da3bd90799fcadf9b2984f
Oz Tiram <otiram@genesiscloud.com>
2020-05-21 14:50:06 +0200

    Use type(...) to create an object to all resources
    This is quite fancy! We can use meta programming to define classes
    for all the resources.

--- src/pygc/client.py
+++ src/pygc/client.py
@@ -1,11 +1,62 @@
+import sys
+
 import requests
+
+RESOURCES = {'Images': 'images',
+             'Instances': 'instances',
+             'SSHKeys': 'ssh-keys',
+             'SecurityGroups': 'security-groups',
+             'Snapshots': 'snapshots'}
+
+
+class GenesisResource:
+
+    def __init__(self, apikey):
+        self.base_url = "https://api.genesiscloud.com/"
+        self.apikey = apikey
+
+    @property
+    def headers(self):
+        return {"Content-Type": "application/json", "X-Auth-Token": self.apikey}
+
+    def __list(self, page=1, items=10, json=False, raw=False, **kwargs):
+        print(self.base_url + f"compute/v1/{self._route}")
+        response = requests.get(
+                self.base_url + f"compute/v1/{self._route}",
+                headers=self.headers,
+                params={"page": page, "per_page": items}
+                )
+
+        if json:
+            yield response.json()['instances']
+        if raw:
+            yield response.json()
+        else:
+            # TODO: create instance of gc.Instance for each item
+            for item in response.json()[self._route.replace("-", "_")]:
+                yield item
+
+    def list(self, page=1, items=10, json=False, raw=False):
+
+        if json:
+            return next(self.__list(page=page, items=items, json=True))
+        if raw:
+            return next(self.__list(page=page, items=items, raw=True))
+        else:
+            return self.__list(page=page, items=items)
+
+
+for resource, route in RESOURCES.items():
+    locals()[resource] = type(resource,
+                              (GenesisResource, object),
+                              {"_route": route})
 
 
 class Client:
 
     def __init__(self, apikey):
         self.apikey = apikey
-        self.base_url = "http://api.genesiscloud.com/"
+        self.base_url = "https://api.genesiscloud.com/"
 
     @property
     def headers(self):
@@ -23,33 +74,5 @@ class Client:
 
         return r
 
-    def _instances(self, page=1, items=10, json=False, raw=False):
-        response = requests.get(self.base_url + "compute/v1/instances",
-                                headers=self.headers,
-                                params={"page": page, "per_page": items}
-                                )
-        if json:
-            yield response.json()['instances']
-        if raw:
-            yield response.json()
-        else:
-            # TODO: create instance of gc.Instance for each item
-            for item in response.json()['instances']:
-                yield item
-
-    def instances(self, page=1, items=10, json=False, raw=False):
-        """
-        List instances
-
-        Keywords:
-            page (int): page number
-            items (int): items per page
-            json (bool): return json response
-            raw (bool): return raw json response
-        """
-        if json:
-            return next(self._instances(page=page, items=items, json=True))
-        if raw:
-            return next(self._instances(page=page, items=items, raw=True))
-        else:
-            return self._instances(page=page, items=items)
+    def __getattr__(self, name):
+        return getattr(sys.modules[__name__], name)(self.apikey)
