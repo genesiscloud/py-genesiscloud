385cac2693c92581aca87b1f98b6c9d76c6c7fbb
Oz Tiram <otiram@genesiscloud.com>
2020-05-28 19:50:57 +0200

    Add better version detection mechanism

--- src/genesiscloud/client.py
+++ src/genesiscloud/client.py
@@ -5,6 +5,12 @@ import requests
 
 from munch import Munch
 
+try:
+    from pkg_resources import get_distribution, DistributionNotFound
+    __version__ = get_distribution("genesiscloud").version
+except (ImportError, DistributionNotFound):
+    __version__ = "0.2.0"
+
 
 class APIError(Exception):
     """An API Error Exception"""
