eaecbebd4e0246985e65c6a05123fe6c83424cf3
Oz Tiram <otiram@genesiscloud.com>
2020-06-16 18:00:17 +0200

    Bump version to 0.2.1

--- src/genesiscloud/__init__.py
+++ src/genesiscloud/__init__.py
@@ -26,4 +26,4 @@ try:
     from pkg_resources import get_distribution, DistributionNotFound
     __version__ = get_distribution("genesiscloud").version
 except (ImportError, DistributionNotFound):
-    __version__ = "0.2.0"
+    __version__ = "0.2.1"
